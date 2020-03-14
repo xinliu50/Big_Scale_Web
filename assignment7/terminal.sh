6d0a4946e5e31020c9a4918c276c6a43fb06ff18
Xin Liu <xinliu253@yahoo.com>
2020-03-14 14:52:08 -0400

    assignment7

---
+++ assignment7/terminal.sh
@@ -0,0 +1,124 @@
+ 
+   82  which svn
+   83  svn --version
+   84  svnadmin
+   85  cd desktop
+   86  mkdir
+   87  mkdir Assignment7
+   88  cd Assignment7/
+   89  pwd
+   90  cd 
+   91  svnadmin create /Users/xinliu/desktop/Assignment7
+   92  python version
+   93  python -version
+   94  python -v
+   95  ifconfig
+   96  cd desktop
+   97  mkdir Assignment7
+   98  cd Assignment7/
+   99  pwd
+  100  cd
+  101  cd desktop
+  102  mkdir hello_world
+  103  cd hello_world/
+  104  vi hello.py
+  105  python hello.py
+  106  cd ..
+  107  svnadmin create /Users/xinliu/desktop/Assignment7/hello
+  108  svnserve -d -r /Users/xinliu/desktop/Assignment7
+  109  svn import hello_world/ svn://127.0.0.1/hello
+  110  svn import hello_world/ svn://127.0.0.1/hello
+  111  export EDITOR
+  112  svn import hello_world/ svn://127.0.0.1/hello
+  113  export SVN_EDITOR=emacs
+  114  svn import hello_world/ svn://127.0.0.1/hello
+  115  cd ..
+  116  cd ..
+  117  ls
+  118  cd etc
+  119  ls
+  120  cd
+  121  ls
+  122  cd usr
+  123  cd ..
+  124  ls
+  125  cd ..
+  126  ls
+  127  cd usr
+  128  ls
+  129  cs share/
+  130  cd share/
+  131  ls
+  132  cd vim
+  133  ls
+  134  cd vim80
+  135  ls
+  136  pwd
+  137  cd
+  138  history
+  139  export SVN_EDITOR="/usr/share/vim/vim80"
+  140  svn import hello_world/ svn://127.0.0.1/hello
+  141  export SVN_EDITOR="/usr/share/vim"
+  142  svn import hello_world/ svn://127.0.0.1/hello
+  143  export VISUAL=vim
+  144  export EDITOR="$VISUAL"
+  145  svn import hello_world/ svn://127.0.0.1/hello
+  146  export VISUAL=vim
+  147   export SVN_EDITOR=VISUAL
+  148  svn import hello_world/ svn://127.0.0.1/hello
+  149  svn commit -m "commit"
+  150  cd deskt
+  151  cd desktop
+  152  cd hello_world/
+  153  svn commit -m "commit"
+  154  cd ..
+  155  cd Assignment7/
+  156  svn commit -m "commit"
+  157  cd
+  158  cd desktop
+  159  cd hello_world/
+  160  cd ..
+  161  svn import hello_world/ svn://127.0.0.1/hello
+  162  export SVN_EDITOR=emacs
+  163  svn import hello_world/ svn://127.0.0.1/hello
+  164  rm *temp
+  165  rm *tmp
+  166  cd workingFile/
+  167  pwd
+  168  ls
+  169    112  svn import hello_world/ svn://127.0.0.1/hello
+  170  svn import hello_world/ svn://127.0.0.1/hello
+  171  ls
+  172  rm -rf hello_world/
+  173  ls
+  174  cd *tmp
+  175  rm *tmp
+  176  svn checkout svn://127.0.0.1/hello
+  177  ls
+  178  vi hello.py
+  179  ls
+  180  cd hello/
+  181  ls
+  182  vi hello.py
+  183  svn commit -m "my second commit"
+  184  s
+  185  ls
+  186  cd ..
+  187  ls
+  188  vim Makefile
+  189  ls
+  190  svn checkout
+  191  checout
+  192  checkout
+  193  vi Makefile 
+  194  make
+  195  ls
+  196  python hello.py
+  197  cd hello/
+  198  ls
+  199  Makefile
+  200  vim Makefile
+  201  make
+  202  ls
+  203  vi Makefile
+  204  make
